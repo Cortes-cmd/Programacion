13dc4efb2509d41ed0e3e9213b03eea4afa5a87b
Cortes-cmd <alejandro.cortesdiaz24@campusfp.es>
2025-03-17 14:46:11 +0100

    a

---
+++ 3ºTrimestre/Actividades/Java 0/Java/Ej2/MiNombre.java
@@ -0,0 +1,12 @@
+
+public class MiNombre {
+    public static void main(String[] args) {
+        System.out.println("¡Hola,Alejandro!");
+	MiNombre nombre = new MiNombre();
+	nombre.saludar();
+    }
+
+    public void saludar(){
+	System.out.println("!Bienvenido a Java!");
+	}
+}
---
+++ 3ºTrimestre/Actividades/Java 0/Java/Ej4/misUtilidades/Mensaje.java
@@ -0,0 +1,10 @@
+package misUtilidades;
+
+public class  saludo{
+
+	public void saludo(String[] args){
+
+		System.out.println("Yeyo yeyo");
+
+	}
+}
