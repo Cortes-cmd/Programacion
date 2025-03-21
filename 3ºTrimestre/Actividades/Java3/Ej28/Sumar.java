da4695ed1a3ba4f602fa0322ab8da90a01454b36
Cortes-cmd <alejandro.cortesdiaz24@campusfp.es>
2025-03-21 10:47:30 +0100

    ñ

--- 3ºTrimestre/Actividades/Java3/Ej28/Sumar.java
+++ 3ºTrimestre/Actividades/Java3/Ej28/Sumar.java
@@ -7,21 +7,23 @@ public class Sumar{
 	public static void main(String[] args){
 
 	 Scanner scanner = new Scanner(System.in);
+	
+	int suma=0;
+	int numero;
+	
 
         do {
 
-	    System.out.println("Ingresa un número y lo sumaré");		    int suma = scanner.nextInt();
+	    System.out.println("Ingresa un número y lo sumaré , Deseas Salir? Si = escribe 0");		    
+	numero = scanner.nextInt();
 		
-	    numero+=suma;
+	    suma+=numero;
 
             System.out.println("El número elegido es; " + numero + "De momento el resultado de la suma es;" + suma);
 
-	    System.out.println("Deseas continuar introduciendo números? Si = cualquier número/ No = 0");
-
-	    int detencion = scanner.nextInt();
-
+	    
             
-        } while (detencion != 0);
+        } while (numero != 0);
 
     	}
 
