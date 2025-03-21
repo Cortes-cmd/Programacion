857c3e9bc132bcceb4869cb80bd9d13b48a2e66f
Cortes-cmd <alejandro.cortesdiaz24@campusfp.es>
2025-03-21 15:07:16 +0100

    s

---
+++ 3ºTrimestre/Actividades/Java3/Ej29/bucleFor.java
@@ -0,0 +1,38 @@
+public class bucleFor{
+
+
+	
+	public static void main (String [] args){
+
+	
+		for (int i = 1; i <=10; i++){
+			
+		System.out.println("El numero actual es ; " + i);		
+	
+		if(i ==5){
+		
+		System.out.println("Saltando el 5... ");
+		continue;		
+	
+		}
+		
+		if (i == 8){
+
+		System.out.println("Se detecta 8, saliendo...");
+		break;		
+	
+		}
+	
+		}
+	
+
+	}
+
+
+
+
+
+
+
+
+}
---
+++ 3ºTrimestre/Actividades/Java3/Ej30/Verificador.java
@@ -0,0 +1,30 @@
+
+public class Verificador{
+
+
+	public static void main(String[] args){
+
+	System.out.println(esPositivoYPar(6));
+
+	}
+
+	public static boolean esPositivoYPar(int numero){
+
+	if (numero %2==0 && numero >=0){
+	
+	return true;
+	} else {
+	
+	return false;
+	
+	}
+
+	}
+
+
+
+
+
+
+
+}
